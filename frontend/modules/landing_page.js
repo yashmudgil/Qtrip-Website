363ba6ae8c9614d5a05b5d8119c3f6ea36656fd3
Yash Mudgil <52819045+yashmudgil@users.noreply.github.com>
2022-01-25 22:26:25 +0530

    Update landing_page.js

--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -3,6 +3,7 @@ import config from "../conf/index.js";
 async function init() {
   //Fetches list of all cities along with their images and description
   let cities = await fetchCities();
+
   //Updates the DOM with the cities
   cities.forEach((key) => {
     addCityToDOM(key.id, key.city, key.description, key.image);
@@ -34,19 +35,19 @@ function addCityToDOM(id, city, description, image) {
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
   const data = document.getElementById("data");
-  const cityCard = document.createElement("div");
-  cityCard.setAttribute("class", "col-12 col-sm-6 col-lg-3 mb-4");
-  const card = `<a id="${id}" href="pages/adventures/?city=${id}">
-    <div class="tile">
-      <img src="${image}" alt="${city}" class="img-fluid">
-      <div class="tile-text text-center text-white">
-        <h2>${city}</h2>
-        <p>${description}</p>
-      </div>
+  const cc = document.createElement("div");
+  cc.className="col-6 col-lg-3 mb-4";
+  cc.innerHTML=`<a href="pages/adventures/?city=${id}" id="${id}">
+  <div class="tile">
+    <div class="tile-text text-center">
+      <h5>${city}</h5>
+      <p>${description}</p>
     </div>
-  </a>`;
-  cityCard.innerHTML = card;
-  data.appendChild(cityCard);
+    <img class="img-responsive" src="${image}" />
+  </div>
+  </a>
+  `;
+  data.appendChild(cc)
 }
 
 export { init, fetchCities, addCityToDOM };
